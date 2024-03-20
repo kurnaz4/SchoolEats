d458dd45aff30127a39f82196a1ccee1a7a2fd71
Gogi4aaa <georgi-07@abv.bg>
2024-03-20 20:49:35 +0200

    installed correct nuget packages

--- SchoolEats/SchoolEats.Data.Models/obj/Debug/net6.0/SchoolEats.Data.Models.AssemblyInfo.cs
+++ SchoolEats/SchoolEats.Data.Models/obj/Debug/net6.0/SchoolEats.Data.Models.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolEats.Data.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85730c878a831597eb3920b30aa46fec81f467fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolEats.Data.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolEats.Data.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
