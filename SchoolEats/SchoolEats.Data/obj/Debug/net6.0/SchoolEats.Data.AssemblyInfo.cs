3f5bf08ab5d5d456d032699d2c13d36bd4e91edc
Marto <kornazov04@gmail.com>
2024-03-20 21:44:08 +0200

    add somethig

--- SchoolEats/SchoolEats.Data/obj/Debug/net6.0/SchoolEats.Data.AssemblyInfo.cs
+++ SchoolEats/SchoolEats.Data/obj/Debug/net6.0/SchoolEats.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolEats.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85730c878a831597eb3920b30aa46fec81f467fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolEats.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolEats.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
