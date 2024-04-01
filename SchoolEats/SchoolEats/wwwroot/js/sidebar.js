e0080be9668c6d984939239add0dbcaa371035eb
Gogi4aaa <georgi-07@abv.bg>
2024-04-01 18:56:34 +0300

    fixed sidebar and main layout width

--- SchoolEats/SchoolEats/wwwroot/js/sidebar.js
+++ SchoolEats/SchoolEats/wwwroot/js/sidebar.js
@@ -4,26 +4,26 @@ var searchBtn = document.querySelector(".bx-search");
 var div = document.querySelector("#main-div-test");
 
 closeBtn.addEventListener("click", () => {
-    div.removeClass("app-sidebar-margin");
-    div.addClass("app-sidebar-margin-more");
+    div.classList.remove("app-sidebar-margin");
+    div.classList.add("app-sidebar-margin-more");
     sidebar.classList.toggle("open");
     menuBtnChange();
 });
 searchBtn.addEventListener("click", () => {
-    div.removeClass("app-sidebar-margin");
-    div.addClass("app-sidebar-margin-more");
+    div.classList.remove("app-sidebar-margin");
+    div.classList.add("app-sidebar-margin-more");
     sidebar.classList.toggle("open");
     menuBtnChange();
 });
 
 function menuBtnChange() {
     if (sidebar.classList.contains("open")) {
-        //div.removeClass("app-sidebar-margin");
-        //div.addClass("app-sidebar-margin-more");
+        div.classList.remove("app-sidebar-margin");
+        div.classList.add("app-sidebar-margin-more");
         closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
     } else {
-        //div.removeClass("app-sidebar-margin-more");
-        //div.addClass("app-sidebar-margin");
+        div.classList.remove("app-sidebar-margin-more");
+        div.classList.add("app-sidebar-margin");
         closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");
     }
 }
