b7e962471ed5c2b856d1ef49e48202f7b086166f
Marto <kornazov04@gmail.com>
2024-04-03 22:58:46 +0300

    add changes in code

--- SchoolEats/SchoolEats/Program.cs
+++ SchoolEats/SchoolEats/Program.cs
@@ -46,7 +46,7 @@ if (app.Environment.IsDevelopment())
     app.UseMigrationsEndPoint();
     app.SeedAdministrator(GeneralApplicationConstants.DevelopmentAdminEmail);
     app.SeedUser();
-    app.SeedSuperUser();
+    app.SeedSuperUser();    
 }
 else
 {
