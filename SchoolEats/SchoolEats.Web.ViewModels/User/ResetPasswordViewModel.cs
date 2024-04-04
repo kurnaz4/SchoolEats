ed8aa89fc9a70b80d9217928a3b5ed56a55b8a46
Gogi4aaa <georgi-07@abv.bg>
2024-04-05 02:36:12 +0300

    add forgot password functionality

---
+++ SchoolEats/SchoolEats.Web.ViewModels/User/ResetPasswordViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace SchoolEats.Web.ViewModels.User
+{
+	using System.ComponentModel.DataAnnotations;
+
+	public class ResetPasswordViewModel
+	{
+		[Display(Name = "Парола")]
+		public string Password { get; set; }
+
+		[Display(Name = "Потвърди парола")]
+		public string ConfirmPassword { get; set; }
+
+		public string Token { get; set; }
+
+		public string Email { get; set; }
+	}
+}
--- SchoolEats/SchoolEats/Controllers/UserController.cs
+++ SchoolEats/SchoolEats/Controllers/UserController.cs
@@ -10,29 +10,33 @@ namespace SchoolEats.Controllers
 	using static Common.NotificationMessagesConstants;
 	using static Common.ErrorMessages;
 	using static Common.GeneralApplicationConstants;
+	using static System.Net.WebRequestMethods;
+
 	public class UserController : Controller
 	{
 		private readonly IUserService userService;
 		private readonly IEmailSender emailSender;
 		private readonly UserManager<SchoolEatsUser> userManager;
-	    public UserController(IUserService userService, IEmailSender emailSender, UserManager<SchoolEatsUser> userManager)
-	    {
-		    this.userService = userService;
+
+		public UserController(IUserService userService, IEmailSender emailSender,
+			UserManager<SchoolEatsUser> userManager)
+		{
+			this.userService = userService;
 			this.emailSender = emailSender;
 			this.userManager = userManager;
-	    }
+		}
 
-        [HttpGet]
-        public IActionResult RegisterConfirmation(string returnUrl)
-        {
-            return View();
-        }
+		[HttpGet]
+		public IActionResult RegisterConfirmation(string returnUrl)
+		{
+			return View();
+		}
 
-        [HttpPost]
-        public async Task<IActionResult> ApproveUser(Guid userId)
-        {
-	        try
-	        {
+		[HttpPost]
+		public async Task<IActionResult> ApproveUser(Guid userId)
+		{
+			try
+			{
 				await this.userService.ApproveUserAsync(userId);
 
 				var user = await this.userService.GetUserAsync(userId);
@@ -41,89 +45,166 @@ namespace SchoolEats.Controllers
 					"Благодарим ви за вашата регистрация в SchoolEats. Акаунтът ви е успешно удобрен!");
 
 				TempData[SuccessMessage] = "Вие успешно удобрихте този потребител!";
-	        }
-	        catch (Exception e)
-	        {
-		        TempData[ErrorMessage] = CommonErrorMessage;
-	        }
+			}
+			catch (Exception e)
+			{
+				TempData[ErrorMessage] = CommonErrorMessage;
+			}
 
-	        return RedirectToAction("PendingUsers", "Admin");
-        }
+			return RedirectToAction("PendingUsers", "Admin");
+		}
 
-        [HttpPost]
-        public async Task<IActionResult> RemoveUser(Guid userId)
-        {
-	        try
-	        {
-		        var user = await this.userService.GetUserAsync(userId);
-		       
+		[HttpPost]
+		public async Task<IActionResult> RemoveUser(Guid userId)
+		{
+			try
+			{
+				var user = await this.userService.GetUserAsync(userId);
+
 				await this.userService.RemoveUserAsync(userId);
 				await this.emailSender.SendEmailAsync(user.Email, "Отхвърлен акаунт от School Eats",
 					"Вашият акаунт е отхвърлен поради опит за повторно влизане на един и същ потребител в системата или поради някаква съмнителна грешка!");
 
 				TempData[SuccessMessage] = "Успешно премахнахте този потребител!";
-	        }
-	        catch (Exception e)
-	        {
-				TempData[ErrorMessage] = CommonErrorMessage;
-	        }
-
-	        return RedirectToAction("PendingUsers", "Admin");
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> AddUserToSuperUserRole(Guid userId)
-        {
-	        try
-	        {
-
-		        var user = await this.userService.AddUserToSuperUserRoleAsync(userId);
-		        
-		        await emailSender.SendEmailAsync(user.Email, "Екип готвачи в SchoolEats",
-			        "Акаунтът ви е успешно добавен към готвачите и вече имате всички права на един готвач!");
-
-		        TempData[SuccessMessage] = "Вие успешно добавихте този потребител към готвачите!";
 			}
-	        catch (Exception e)
-	        {
+			catch (Exception e)
+			{
 				TempData[ErrorMessage] = CommonErrorMessage;
 			}
 
-	        return RedirectToAction("AllUsers", "Admin");
+			return RedirectToAction("PendingUsers", "Admin");
 		}
 
-        [HttpPost]
-        public async Task<IActionResult> RemoveUserFromSuperUserRole(Guid userId)
-        {
-	        try
-	        {
-		        var user = await this.userService.RemoveSuperUserRoleFromUserAsync(userId);
+		[HttpPost]
+		public async Task<IActionResult> AddUserToSuperUserRole(Guid userId)
+		{
+			try
+			{
 
-		        await emailSender.SendEmailAsync(user.Email, "Екип готвачи в SchoolEats",
-			        "Акаунтът ви е успешно добавен към готвачите и вече имате всички права на един готвач!");
+				var user = await this.userService.AddUserToSuperUserRoleAsync(userId);
 
-		        TempData[SuccessMessage] = "Вие успешно премахнахте този потребител от готвачите!";
-		        TempData[InformationMessage] = "Потребителят вече е без права на готвач!";
-	        }
-	        catch (Exception e)
-	        {
-		        TempData[ErrorMessage] = CommonErrorMessage;
-	        }
+				await emailSender.SendEmailAsync(user.Email, "Екип готвачи в SchoolEats",
+					"Акаунтът ви е успешно добавен към готвачите и вече имате всички права на един готвач!");
 
-	        return RedirectToAction("AllUsers", "Admin");
-        }
+				TempData[SuccessMessage] = "Вие успешно добавихте този потребител към готвачите!";
+			}
+			catch (Exception e)
+			{
+				TempData[ErrorMessage] = CommonErrorMessage;
+			}
+
+			return RedirectToAction("AllUsers", "Admin");
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> RemoveUserFromSuperUserRole(Guid userId)
+		{
+			try
+			{
+				var user = await this.userService.RemoveSuperUserRoleFromUserAsync(userId);
+
+				await emailSender.SendEmailAsync(user.Email, "Екип готвачи в SchoolEats",
+					"Акаунтът ви е успешно добавен към готвачите и вече имате всички права на един готвач!");
+
+				TempData[SuccessMessage] = "Вие успешно премахнахте този потребител от готвачите!";
+				TempData[InformationMessage] = "Потребителят вече е без права на готвач!";
+			}
+			catch (Exception e)
+			{
+				TempData[ErrorMessage] = CommonErrorMessage;
+			}
+
+			return RedirectToAction("AllUsers", "Admin");
+		}
 
 		[HttpGet]
-        public IActionResult ForgotPassword()
-        {
+		public IActionResult ForgotPassword()
+		{
 			ForgotPasswordViewModel model = new ForgotPasswordViewModel();
 			return View(model);
-        }
-        [HttpPost]
-        public IActionResult ForgotPassword(ForgotPasswordViewModel model)
-        {
-	        //TODO
-	        return View();
-        }
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> ForgotPassword(ForgotPasswordViewModel model)
+		{
+			//TODO
+			if (!ModelState.IsValid)
+			{
+				return View(model);
+			}
+
+			try
+			{
+				var user = await userManager.FindByEmailAsync(model.Email);
+				if (user == null)
+				{
+					ModelState.AddModelError("Email", "Такъв имейл не съществува в системата");
+					return View(model);
+				}
+
+				var token = await userManager.GeneratePasswordResetTokenAsync(user);
+				var link = "<a href='" +
+				           Url.Action("ResetPassword", "User", new { email = model.Email, code = token }, "https") +
+				           "' target='_blank'>Reset Password</a>";
+
+				await this.emailSender.SendEmailAsync(model.Email, "Промени парола в School Eats",
+					$"Натиснете {link} за да смените вашата парола");
+				TempData[SuccessMessage] = "Успешно заявихте смяна на паролата Ви!";
+				TempData[InformationMessage] = "Проверете имейла си!";
+			}
+			catch (Exception e)
+			{
+				TempData[ErrorMessage] = CommonErrorMessage;
+			}
+
+			return LocalRedirect("/Identity/Account/Login");
+		}
+
+		[HttpGet]
+		public IActionResult ResetPassword(string email, string code)
+		{
+			ResetPasswordViewModel model = new ResetPasswordViewModel();
+			model.Token = code;
+			model.Email = email;
+			return View(model);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> ResetPassword(ResetPasswordViewModel model)
+		{
+			if (!ModelState.IsValid)
+			{
+				ModelState.AddModelError("Password", "Невалидна парола!");
+				ModelState.AddModelError("ConfirmPassword", "Невалидна парола!");
+				return View(model);
+			}
+			
+			try
+			{
+				var user = await userManager.FindByEmailAsync(model.Email);
+				if (user == null)
+					return View(model);
+				var resetPassResult = await userManager.ResetPasswordAsync(user, model.Token, model.Password);
+				if (!resetPassResult.Succeeded)
+				{
+					foreach (var error in resetPassResult.Errors)
+					{
+						ModelState.TryAddModelError(error.Code, error.Description);
+					}
+
+					return View(model);
+				}
+
+				await this.emailSender.SendEmailAsync(user.Email, "Променена парола в School Eats",
+					"Успешно променихте паролата си в нашата система!");
+				TempData[SuccessMessage] = "Успешно променихте паролата си!";
+			}
+			catch (Exception e)
+			{
+				TempData[ErrorMessage] = CommonErrorMessage;
+			}
+			
+			return LocalRedirect("/Identity/Account/Login");
+		}
 	}
 }
