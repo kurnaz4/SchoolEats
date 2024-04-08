54a887f90bb78685d3d8d7521c20cf19596a939d
Gogi4aaa <georgi-07@abv.bg>
2024-04-08 21:09:14 +0300

    fixed add dish validations

--- SchoolEats/SchoolEats.Common/ValidationConstants.cs
+++ SchoolEats/SchoolEats.Common/ValidationConstants.cs
@@ -10,11 +10,11 @@
         public static class Dish
 		{
 			public const int NameMaxLength = 30;
-			public const int NameMinLength = 5;
+			public const int NameMinLength = 2;
 			public const int DescriptionMaxLength = 200;
-			public const int DescriptionMinLength = 20;
+			public const int DescriptionMinLength = 10;
 			public const int ImageUrlMaxLength = 1000;
-			public const decimal PriceMinLength = 0.10m;
+			public const decimal PriceMinLength = 0.5m;
 			public const decimal PriceMaxLength = 100;
 			public const int QuantityMaxLength = 1000;
 			public const int QuantityMinLength = 1;
